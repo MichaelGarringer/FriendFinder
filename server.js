2477cc27fdf9fc2a9cb2901cab8ed75d790c3928
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-06-05 20:58:44 -0500

    survey results successfully save to the api page upon survey completion, basic css changes

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-
+//Dependency for express
 var express = require("express");
 
 
 var app = express();
 
-
+//Define port
 var PORT = process.env.PORT || 3030;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+//Routing
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
-
+//Run the PORT 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
