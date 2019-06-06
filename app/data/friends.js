8ca9d55d4ae80dc8fa2155ded1387804c90a5e05
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-06-05 22:38:23 -0500

    added very basic modal - doesn't pull the correct info but displays some info currently

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
  var friendsList = [
      {
  name: "Mike",
- photo: "google.com",
+ photo: "https://upload.wikimedia.org/wikipedia/en/5/52/Ned_Stark-Sean_Bean.jpg",
  email: "michaelleelyngarringer@gmail.com",
  testAnswers: [1,2,3,4,5,1,2,3,4,5]
 }
